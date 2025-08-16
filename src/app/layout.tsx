7c83309a98fe2fa187a965f7ba9e576755e94c1f
joebobred <joeredmond@hotmail.co.uk>
2025-08-16 15:32:24 +0100

    Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react'
+import './globals.css'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,13 +23,12 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+return (
+  <html lang="en">
+    <body className={inter.className}>
+      {children}
+      <Analytics />
+    </body>
+  </html>
+);
 }
